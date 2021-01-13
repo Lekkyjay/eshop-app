56c4817dc8da2b6388bcfa19334658e29ba77da8
Lekkyjay <lekkyjayjay@yahoo.com>
2021-01-13 14:33:14 +0100

    Installed and initialized firebase. Generated required components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CheckOutComponent,
+    HomeComponent,
+    LoginComponent,
+    MyOrdersComponent,
+    OrderSuccessComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    AdminOrdersComponent,
+    AdminProductsComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAw9-SSy7f0mnhC2CHZPUd7X5glNVfRwkU",
+    authDomain: "eshop-fcdf4.firebaseapp.com",
+    databaseURL: "https://eshop-fcdf4.firebaseio.com",
+    projectId: "eshop-fcdf4",
+    storageBucket: "eshop-fcdf4.appspot.com",
+    messagingSenderId: "42482324003",
+    appId: "1:42482324003:web:2f992fb678d7f6052e5796"
+  }
 };
 
 /*
