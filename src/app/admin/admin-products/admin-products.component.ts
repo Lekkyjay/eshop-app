fb7bc5fed7f0a5f5c52d1cab991088b16564dbf1
Lekkyjay <lekkyjayjay@yahoo.com>
2021-01-14 13:46:29 +0100

    Unused import removed.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
