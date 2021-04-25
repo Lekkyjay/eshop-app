2d4cbdcad7a9c24e07652bea998a2eb38c774b4d
Lekkyjay <lekkyjayjay@yahoo.com>
2021-04-26 00:54:00 +0200

    Push commit.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAw9-SSy7f0mnhC2CHZPUd7X5glNVfRwkU",
-    authDomain: "eshop-fcdf4.firebaseapp.com",
-    databaseURL: "https://eshop-fcdf4.firebaseio.com",
-    projectId: "eshop-fcdf4",
-    storageBucket: "eshop-fcdf4.appspot.com",
-    messagingSenderId: "42482324003",
-    appId: "1:42482324003:web:2f992fb678d7f6052e5796"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
 
